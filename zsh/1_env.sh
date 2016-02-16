d427902c08bae726bdfbf8f08153fb1432aa34e3
Kenny_김익수 <kenny@podotree.com>
2016-02-16 16:04:30 +0900

    add pyenv

--- zsh/1_env.sh
+++ zsh/1_env.sh
@@ -1,3 +1,18 @@
+#
+# User env
+#
 export MYSQL_HOME=/usr/local/mysql
-export PATH=$PATH:$MYSQL_HOME/bin:.
+export PYENV_ROOT=/usr/local/var/pyenv
+export PATH=$PATH:$MYSQL_HOME/bin:$PYENV_ROOT/bin:.
+
+eval "$(pyenv init -)"
+
+#
+# aliases
+#
 alias vim='/usr/local/bin/vim'
+
+#
+# pyenv virtualenv
+#
+if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
